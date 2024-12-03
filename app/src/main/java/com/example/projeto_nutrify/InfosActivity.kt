62c58252d319c963127972d0282eb9966fa5fb00
jhennyf <jhennyfestudos2021@gmail.com>
2024-12-02 22:10:41 -0300

    :sparkles: feat: adicionando navegação a tela perfil

---
+++ app/src/main/java/com/example/projeto_nutrify/InfosActivity.kt
@@ -0,0 +1,15 @@
+package com.example.projeto_nutrify
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class InfosActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun getLayoutResourceId(): Int {
+        return R.layout.activity_infos
+    }
+}
--- app/src/main/java/com/example/projeto_nutrify/PerfilActivity.kt
+++ app/src/main/java/com/example/projeto_nutrify/PerfilActivity.kt
@@ -1,11 +1,19 @@
 package com.example.projeto_nutrify
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 
 class PerfilActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val imcTextView = findViewById<TextView>(R.id.imcTextView)
+        imcTextView.setOnClickListener {
+            val intent = Intent(this, InfosActivity::class.java)
+            startActivity(intent)
+        }
     }
 
     override fun getLayoutResourceId(): Int {
